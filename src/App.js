08b2153883312caa9ba430db9cae7e317a8f1d89
Paula Waite <pawaite@madisoncollege.edu>
2023-03-24 09:32:36 -0500

    Adding delete functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import { List, Input, Button } from 'antd';
 import 'antd/dist/reset.css';
 import { v4 as uuid } from 'uuid';
 import { listNotes } from './graphql/queries';
-import { createNote as CreateNote } from './graphql/mutations';
+import { 
+    createNote as CreateNote,
+    deleteNote as DeleteNote
+} from './graphql/mutations';
 
 const CLIENT_ID = uuid();
 
@@ -70,6 +73,23 @@ const createNote = async () => {
     }
 };
 
+const deleteNote = async({ id }) => {
+    const index = state.notes.findIndex(n => n.id === id)
+    const notes = [
+      ...state.notes.slice(0, index), // filter?
+      ...state.notes.slice(index + 1)];
+    dispatch({ type: 'SET_NOTES', notes })
+    try {
+      await API.graphql({
+        query: DeleteNote,
+        variables: { input: { id } }
+      })
+      console.log('successfully deleted note!')
+      } catch (err) {
+        console.error(err)
+    }
+  };
+
 const onChange = (e) => {
     dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
   };
@@ -87,7 +107,11 @@ const onChange = (e) => {
 
     function renderItem(item) {
         return (
-            <List.Item style={styles.item}>
+            <List.Item 
+                style={styles.item}
+                actions={[
+                    <p style={styles.p} onClick={() => deleteNote(item)}>Delete</p>
+                  ]}>
             <List.Item.Meta
                 title={item.name}
                 description={item.description}
